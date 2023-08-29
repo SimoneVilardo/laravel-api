bcb0e422b8adb772640fb0e3a18d2d7c3a6614cf
SimoneVilardo <simovila95@gmail.com>
2023-08-29 14:03:42 +0200

    milstone 1 e 2 completata

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index(){
+        $projects = Project::all();
+        return response()->json([
+            
+            'success' => true,
+            'results' => $projects
+
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controller\Api\ProjectController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/projects', [ProjectController::class, 'index']);
