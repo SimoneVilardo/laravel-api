39acf03677a845010e2afbd86c30e680d0028988
SimoneVilardo <simovila95@gmail.com>
2023-08-29 18:12:32 +0200

    mod per la paginazione funz index in projcontrol

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,7 @@ use App\Models\Project;
 class ProjectController extends Controller
 {
     public function index(){
-        $projects = Project::with('type', 'technologies')->get();
+        $projects = Project::with('type', 'technologies')->paginate(3);
         return response()->json([
             
             'success' => true,
