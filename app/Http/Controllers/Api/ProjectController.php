46ca671e1285534686346b6ae206cb33ddd7f2e1
SimoneVilardo <simovila95@gmail.com>
2023-08-31 13:45:30 +0200

    creata rotta e metodo show

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -18,4 +18,20 @@ class ProjectController extends Controller
 
         ]);
     }
+
+    public function show($slug){
+        $project = Project::with('type', 'technologies')->where('slug', $slug)->first();
+
+        if($project){
+            return response()->json([
+                'success' => true,
+                'project' => $project
+            ]);
+        }
+
+        return response()->json([
+            'success' => false,
+            'error'   => 'Nessun progetto trovato'
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/projects/{slug}', [ProjectController::class, 'show']);
